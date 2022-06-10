038b00800a58c22a482b938d439db023fc4b0ac0
Dan Johnson <johnsoad@wfu.edu>
2022-06-10 13:29:58 -0400

    Fix to csv

--- sawhet_v2_0/data-raw/courses.R
+++ sawhet_v2_0/data-raw/courses.R
@@ -1,6 +1,6 @@
 ## code to prepare `course` dataset goes here
 
-df_courses <- "data-raw/courses.csv" |> 
+df_courses <- "data-raw/coursesFa22.csv" |> 
   read.csv()
 
 usethis::use_data(df_courses, internal = TRUE, overwrite = TRUE)
